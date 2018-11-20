48e592ec488e7d3a12121a893365ea702d926cb7
wtong2017 <wtong@connect.ust.hk>
2018-11-21 01:40:58 +0800

    Add glass to some windows

--- www/js/room.js
+++ www/js/room.js
@@ -1,8 +1,23 @@
 class Window {
-    constructor(width, height, position) {
+    constructor(width, height, thickness, position) {
         this.height = height;
         this.width = width;
+        this.thickness = thickness
         this.pos = position;
+        this.mesh = null
+    }
+    createMesh(material) {
+        var mesh = new Physijs.BoxMesh(
+            new THREE.CubeGeometry( this.width, this.thickness, this.height),
+            material,
+            0
+        );
+        this.mesh = mesh;
+    }
+    getMesh() {
+        if (this.mesh == null)
+            return null;
+        return this.mesh;
     }
 }
 
@@ -18,12 +33,19 @@ function buildFloor(width, height, thickness, pos, rot, color) {
 }
 
 function buildWall(width, height, thickness, pos, rot, color, windows) {
+    var allMesh = [];
+
     var wall = buildFloor(width, height, thickness, [0, 0, 0], [0, 0, 0], color);
     var wallCsg = THREE.CSG.fromMesh(wall);
 
     // Subtract window
     for (let i = 0; i < windows.length; i++) {
         var window = buildFloor(windows[i].width, windows[i].height, thickness, [0, 0, 0], [0, 0, 0], color);
+        var windowMesh = windows[i].getMesh();
+        if (windowMesh) {
+            windowMesh.position.set( windows[i].pos[0], windows[i].pos[1], windows[i].pos[2] );
+            allMesh.push(windowMesh);
+        }
         window.position.set( windows[i].pos[0], windows[i].pos[1], windows[i].pos[2] );
         
         var windowCsg = THREE.CSG.fromMesh(window);
@@ -31,8 +53,20 @@ function buildWall(width, height, thickness, pos, rot, color, windows) {
     }
 
     var resultMesh = THREE.CSG.toPhysiMesh(wallCsg, wall.material, 0)
+    // allMesh.push(resultMesh);
+    // allMesh.forEach(element => {
+    //     element.rotation.set( rot[0], rot[1], rot[2] );
+    //     element.position.set( pos[0], pos[1], pos[2] );
+    // });
+    windows.forEach(element => {
+        windowMesh = element.getMesh()
+        if (windowMesh)
+            resultMesh.add(windowMesh);
+    });
+
     resultMesh.rotation.set( rot[0], rot[1], rot[2] );
     resultMesh.position.set( pos[0], pos[1], pos[2] );
+    // return allMesh;
     return resultMesh;
 }
 
@@ -45,20 +79,22 @@ function buildRoom(scene, floorWidth, floorHeight, wallHeight, thickness) {
   
     // Walls
     var doorHeight = 15
-    var frontDoors = [new Window(10, doorHeight, [0, 0, -(wallHeight-doorHeight)/2])]; // NOTE: Since the floor is originally horizontal, we need to modify the z axis for the position of height
-    frontDoors.push(new Window(10, 10, [-20, 0, 0])); // Add a window
-    frontDoors.push(new Window(10, 10, [20, 0, 0])); // Add a window
+    var frontDoors = [new Window(10, doorHeight, thickness, [0, 0, -(wallHeight-doorHeight)/2])]; // NOTE: Since the floor is originally horizontal, we need to modify the z axis for the position of height
+    frontDoors.push(new Window(10, 10, thickness, [-20, 0, 0])); // Add a window
+    frontDoors.push(new Window(10, 10, thickness, [20, 0, 0])); // Add a window
     var front = buildWall(floorWidth, wallHeight, thickness, [0, 0, (floorHeight + thickness)/2], [-Math.PI / 2, 0, 0], 0xffffff, frontDoors);
     var backWindows = [];
     var numOfWindow = 2;
     for (let i = 0; i < numOfWindow; i++) {
         var windowPos = [0, 0, 0];
         windowPos[0] = 0 - floorWidth/2 + floorWidth/(numOfWindow+1)*(i+1);
-        backWindows.push(new Window(10, 10, windowPos));
+        var window = new Window(10, 10, thickness/4, windowPos);
+        window.createMesh(new THREE.MeshLambertMaterial({ color: 0xffffff, transparent: true, opacity: 0.3 }))
+        backWindows.push(window);
     }
     var back = buildWall(floorWidth, wallHeight, thickness, [0, 0, -(floorHeight + thickness)/2], [Math.PI / 2, 0, 0], 0xffffff, backWindows);
-    var right = buildWall(wallHeight, floorHeight, thickness, [(floorWidth + thickness)/2, 0, 0], [0, 0, Math.PI / 2], 0xffffff, [new Window(10, 10, [0,0,0])]);
-    var left = buildWall(wallHeight, floorHeight, thickness, [-(floorWidth + thickness)/2, 0, 0], [0, 0, Math.PI / 2], 0xffffff, [new Window(10, 10, [0,0,0])]);
+    var right = buildWall(wallHeight, floorHeight, thickness, [(floorWidth + thickness)/2, 0, 0], [0, 0, Math.PI / 2], 0xffffff, [new Window(10, 10, thickness, [0,0,0])]);
+    var left = buildWall(wallHeight, floorHeight, thickness, [-(floorWidth + thickness)/2, 0, 0], [0, 0, Math.PI / 2], 0xffffff, [new Window(10, 10, thickness, [0,0,0])]);
   
     scene.add( bottom );
     scene.add( top );
