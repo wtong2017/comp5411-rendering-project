9982c49da4a78fa54330310ef1e2c8f976012ac0
wtong2017 <wtong@connect.ust.hk>
2018-11-20 22:28:24 +0800

    Further refactor

--- www/js/room.js
+++ www/js/room.js
@@ -1,56 +1,32 @@
 function buildRoom(scene, floorWidth, floorHeight, wallHeight, thickness) {
     // Floor 
-    var bottom = new Physijs.BoxMesh(
-      new THREE.CubeGeometry( floorWidth, thickness, floorHeight ),
-      new THREE.MeshPhongMaterial({ color: 0x000000 }),
-      0
-    );
+    var bottom = buildFloor(floorWidth, floorHeight, thickness, [0, -(wallHeight + thickness)/2, 0], [0, 0, 0], 0x000000);
   
     // Cover
-    var top = new Physijs.BoxMesh(
-      new THREE.CubeGeometry( floorWidth, thickness, floorHeight ),
-      new THREE.MeshPhongMaterial({ color: 0x000000 }),
-      0
-    ); 
+    var top = buildFloor(floorWidth, floorHeight, thickness, [0, (wallHeight + thickness)/2, 0], [0, 0, 0], 0x000000);
   
     // Walls
-    var front = new Physijs.BoxMesh(
-      new THREE.CubeGeometry( floorWidth, thickness, wallHeight ),
-      new THREE.MeshPhongMaterial({ color: 0xffffff }),
-      0
-    );
-    var back = new Physijs.BoxMesh(
-      new THREE.CubeGeometry( floorWidth, thickness, wallHeight ),
-      new THREE.MeshPhongMaterial({ color: 0xffffff }),
-      0
-    );
-    var right = new Physijs.BoxMesh(
-      new THREE.CubeGeometry( wallHeight, thickness, floorHeight ),
-      new THREE.MeshPhongMaterial({ color: 0xffffff }),
-      0
-    );
-    var left = new Physijs.BoxMesh(
-      new THREE.CubeGeometry( wallHeight, thickness, floorHeight ),
-      new THREE.MeshPhongMaterial({ color: 0xffffff }),
-      0
-    );
-    front.rotation.x += Math.PI/2;
-    back.rotation.x += Math.PI/2;
-    right.rotation.z += Math.PI/2;
-    left.rotation.z += Math.PI/2;
-    
-    bottom.position.y -= (wallHeight + thickness)/2;
-    top.position.y += (wallHeight + thickness)/2;
-    front.position.z += (floorHeight + thickness)/2;
-    back.position.z -= (floorHeight + thickness)/2;
-    right.position.x += (floorWidth + thickness)/2;
-    left.position.x -= (floorWidth + thickness)/2;
+    var front = buildFloor(floorWidth, wallHeight, thickness, [0, 0, (floorHeight + thickness)/2], [Math.PI / 2, 0, 0], 0xffffff);
+    var back = buildFloor(floorWidth, wallHeight, thickness, [0, 0, -(floorHeight + thickness)/2], [Math.PI / 2, 0, 0], 0xffffff);
+    var right = buildFloor(wallHeight, floorHeight, thickness, [(floorWidth + thickness)/2, 0, 0], [0, 0, Math.PI / 2], 0xffffff);
+    var left = buildFloor(wallHeight, floorHeight, thickness, [-(floorWidth + thickness)/2, 0, 0], [0, 0, Math.PI / 2], 0xffffff);
   
     scene.add( bottom );
     scene.add( top );
     scene.add( front );
-    scene.add( back);
+    scene.add( back );
     scene.add( right );
     scene.add( left );
     return [bottom, top, front, back, right, left]
-  }
+}
+
+function buildFloor(width, height, thickness, pos, rot, color) {
+    var floor = new Physijs.BoxMesh(
+        new THREE.CubeGeometry( width, thickness, height ),
+        new THREE.MeshPhongMaterial({ color: color }),
+        0
+    );
+    floor.rotation.set( rot[0], rot[1], rot[2] );
+    floor.position.set( pos[0], pos[1], pos[2] );
+    return floor;
+}
